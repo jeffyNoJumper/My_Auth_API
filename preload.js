af50289db89c642f3359e26f781d3bb10970c926
Member <burrellmc43@gmail.com>
2026-02-11 12:12:16 -0600

    Update preload.js
    Added new feed

--- preload.js
+++ preload.js
@@ -1,17 +1,21 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
-
+    // --- Existing C++ & Window Logic ---
     getMachineIdentifier: () => ipcRenderer.invoke('get-hwid'),
     startSpoof: () => ipcRenderer.invoke('start-spoof'),
     launchCheat: (gameName) => ipcRenderer.invoke('launch-game', gameName),
-
     close: () => ipcRenderer.send('window-close'),
     minimize: () => ipcRenderer.send('window-minimize'),
 
+    // --- NEW: Update & News Functionality ---
+    startUpdateDownload: () => ipcRenderer.invoke('start-update-download'),
+    getNews: () => ipcRenderer.invoke('get-news'),
+
     // --- Auto-Updater Listeners (Main.js -> UI.js) ---
+    // Updated to receive version AND news/release notes from GitHub
     onUpdateAvailable: (callback) => 
-        ipcRenderer.on('update-available', (event, version) => callback(version)),
+        ipcRenderer.on('update-available', (event, data) => callback(data)),
         
     onDownloadProgress: (callback) => 
         ipcRenderer.on('download-progress', (event, percent) => callback(percent)),
