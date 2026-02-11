db70f5f8e98ad311fe1e62135f1a95bcf1b55504
Member <burrellmc43@gmail.com>
2026-02-11 06:58:34 -0600

    Update index.js

--- core/index.js
+++ core/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const User = require('./User');
+const User = require('.../src/user');
 const jwt = require('jsonwebtoken');
 
 const app = express();
