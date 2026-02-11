ac98a18d74c06c0e5c9a2799649b0fcda898fb21
Member <burrellmc43@gmail.com>
2026-02-11 08:49:37 -0600

    Update binding.cpp

--- binding.cpp
+++ binding.cpp
@@ -1,10 +1,16 @@
 #include <napi.h>
-#include "spoofer.cpp" // Ensure your spoofer logic is in this file
+#include "spoofer.cpp" // Contains your HWID and Spoofer logic
 
+// 1. Fetch HWID from C++ to JS
+Napi::String GetHWID(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    std::string hwid = GetMachineID(); // Function from spoofer.cpp
+    return Napi::String::New(env, hwid);
+}
+
+// 2. Trigger Spoofer
 Napi::Value RunSpoofer(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
-    
-    // Execute the functions from your spoofer.cpp
     bool disk = SpoofDisk();
     bool guid = SpoofGUID();
     bool mac = SpoofMAC();
@@ -13,12 +19,24 @@ Napi::Value RunSpoofer(const Napi::CallbackInfo& info) {
     result.Set("disk", Napi::Boolean::New(env, disk));
     result.Set("guid", Napi::Boolean::New(env, guid));
     result.Set("mac", Napi::Boolean::New(env, mac));
-
     return result;
 }
 
+// 3. Trigger Injection/Launch
+Napi::Boolean LaunchCheat(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    std::string gameName = info[0].As<Napi::String>().Utf8Value();
+    
+    // Logic: If FiveM, call specific function, etc.
+    // Example: bool success = Injector::Load(gameName);
+    bool success = true; // Placeholder for your .dll injection logic
+    return Napi::Boolean::New(env, success);
+}
+
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-    exports.Set(Napi::String::New(env, "runSpoofer"), Napi::Function::New(env, RunSpoofer));
+    exports.Set("getMachineID", Napi::Function::New(env, GetHWID));
+    exports.Set("runSpoofer", Napi::Function::New(env, RunSpoofer));
+    exports.Set("launchCheat", Napi::Function::New(env, LaunchCheat));
     return exports;
 }
 
