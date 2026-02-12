fe84ada5d353e2b3a068c1b4b0eaa43e58316a1f
Member <burrellmc43@gmail.com>
2026-02-11 18:46:10 -0600

    Update user.js

--- core/user.js
+++ core/user.js
@@ -4,7 +4,10 @@ const UserSchema = new mongoose.Schema({
     license_key: { type: String, required: true, unique: true },
     hwid: { type: String, default: null },
     expiry_date: { type: Date, required: true },
+
     is_banned: { type: Boolean, default: false },
+    is_paused: { type: Boolean, default: false },
+
     games: { type: [String], default: ["FiveM", "GTAV", "Warzone", "CS2"] },
     profile_pic: { type: String, default: "https://i.imgur.com" }
 });
