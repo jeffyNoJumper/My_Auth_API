fe48c24ca8cd3f39eada68018d6d63a4f1e13ebf
Member <burrellmc43@gmail.com>
2026-02-13 17:42:48 -0600

    Update binding.cpp

--- loader/binding.cpp
+++ loader/binding.cpp
@@ -3,10 +3,17 @@
 #include <string>
 #include <random>
 
-// --- CORE LOGIC (Moved from spoofer.cpp) ---
+// These functions come from your user/main.c and kernel/main.c
+extern "C" {
+    bool run_user_spoof();
+    bool run_kernel_spoof();
+}
+
+// --- CORE LOGIC ---
 
 std::string GetMachineID() {
     DWORD serialNum = 0;
+    // Uses C drive serial as a basic HWID
     if (GetVolumeInformationA("C:\\", NULL, 0, &serialNum, NULL, NULL, NULL, 0)) {
         return std::to_string(serialNum);
     }
@@ -25,8 +32,9 @@ std::string GenerateRandomString(int length) {
 
 bool SpoofDisk() {
     HKEY hKey;
+    // Note: Using KEY_WOW64_64KEY to ensure we hit the real registry on 64-bit systems
     const char* path = "HARDWARE\\DEVICEMAP\\Scsi\\Scsi Port 0\\Scsi Bus 0\\Target Id 0\\Logical Unit Id 0";
-    if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, path, 0, KEY_SET_VALUE, &hKey) == ERROR_SUCCESS) {
+    if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, path, 0, KEY_SET_VALUE | KEY_WOW64_64KEY, &hKey) == ERROR_SUCCESS) {
         std::string newSerial = GenerateRandomString(16);
         RegSetValueExA(hKey, "SerialNumber", 0, REG_SZ, (const BYTE*)newSerial.c_str(), (DWORD)newSerial.length());
         RegCloseKey(hKey);
@@ -37,7 +45,7 @@ bool SpoofDisk() {
 
 // --- N-API WRAPPERS ---
 
-Napi::String GetHWID(const Napi::CallbackInfo& info) {
+Napi::String GetHWIDWrapper(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
     return Napi::String::New(env, GetMachineID());
 }
@@ -45,20 +53,30 @@ Napi::String GetHWID(const Napi::CallbackInfo& info) {
 Napi::Value RunSpoofer(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
     Napi::Object result = Napi::Object::New(env);
-    result.Set("disk", Napi::Boolean::New(env, SpoofDisk()));
-    // Adding placeholders for GUID/MAC so JS doesn't crash
-    result.Set("guid", Napi::Boolean::New(env, true));
+
+    // Run the logic from your external C folders
+    bool userStatus = run_user_spoof();
+    bool kernelStatus = run_kernel_spoof();
+    bool diskStatus = SpoofDisk();
+
+    // Mapping results to what your frontend JS expects
+    result.Set("user", Napi::Boolean::New(env, userStatus));
+    result.Set("kernel", Napi::Boolean::New(env, kernelStatus));
+    result.Set("disk", Napi::Boolean::New(env, diskStatus));
+    result.Set("guid", Napi::Boolean::New(env, userStatus));
     result.Set("mac", Napi::Boolean::New(env, true));
+
     return result;
 }
 
 Napi::Value LaunchCheat(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
+    // Your launch logic here
     return Napi::Boolean::New(env, true);
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-    exports.Set("getMachineID", Napi::Function::New(env, GetHWID));
+    exports.Set("getMachineID", Napi::Function::New(env, GetHWIDWrapper));
     exports.Set("runSpoofer", Napi::Function::New(env, RunSpoofer));
     exports.Set("launchCheat", Napi::Function::New(env, LaunchCheat));
     return exports;
