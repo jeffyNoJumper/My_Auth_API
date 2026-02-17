a405641a99c1de61c44b989356af3a0d62aaedaa
Member <burrellmc43@gmail.com>
2026-02-16 22:37:10 -0600

    Update preload.js

--- loader/preload.js
+++ loader/preload.js
@@ -1,25 +1,28 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
-    // --- Existing C++ & Window Logic ---
-    getMachineIdentifier: () => ipcRenderer.invoke('get-hwid'),
-    startSpoof: () => ipcRenderer.invoke('start-spoof'),
-    launchCheat: (gameName) => ipcRenderer.invoke('launch-game', gameName),
+    launchCheat: (gameName, autoClose) => ipcRenderer.invoke('launch-game', gameName, autoClose),
+    toggleStreamProof: (enabled) => ipcRenderer.send('toggle-stream-proof', enabled),
+    toggleDiscord: (enabled) => ipcRenderer.send('toggle-discord', enabled),
+    getNews: () => ipcRenderer.invoke('get-news'),
+
+    // Spoofer Functions (Correctly routed via IPC)
+    startSpoof: (options) => ipcRenderer.invoke('start-spoof', options),
+    getMachineID: () => ipcRenderer.invoke('get-machine-id'),
+    getBaseboard: () => ipcRenderer.invoke('get-baseboard'),
+    getGPUID: () => ipcRenderer.invoke('get-gpuid'),
+
+    // Window Controls
     close: () => ipcRenderer.send('window-close'),
     minimize: () => ipcRenderer.send('window-minimize'),
 
-    // --- NEW: Update & News Functionality ---
-    startUpdateDownload: () => ipcRenderer.invoke('start-update-download'),
-    getNews: () => ipcRenderer.invoke('get-news'),
-
-    // --- Auto-Updater Listeners (Main.js -> UI.js) ---
-    // Updated to receive version AND news/release notes from GitHub
-    onUpdateAvailable: (callback) =>
-        ipcRenderer.on('update-available', (event, data) => callback(data)),
-
+    // Update Listeners
+    onUpdateAvailable: (callback) => {
+        ipcRenderer.removeAllListeners('update-available');
+        ipcRenderer.on('update-available', (event, data) => callback(data));
+    },
     onDownloadProgress: (callback) =>
         ipcRenderer.on('download-progress', (event, percent) => callback(percent)),
-
     onUpdateReady: (callback) =>
         ipcRenderer.on('update-ready', (event) => callback())
 });
