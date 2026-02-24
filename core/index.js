9b800e85458b8212ff59395973106921a3025419
Member <burrellmc43@gmail.com>
2026-02-23 22:11:53 -0600

    Update index.js

--- core/index.js
+++ core/index.js
@@ -8,9 +8,11 @@ const cors = require('cors');
 
 const app = express();
 
+app.use(express.json({ limit: '75mb' }));
+app.use(express.urlencoded({ limit: '75mb', extended: true }));
+
 // Middleware
 app.use(cors());
-app.use(express.json());
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URL)
