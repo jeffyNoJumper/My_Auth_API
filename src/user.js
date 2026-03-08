7c71af5c798953407c07e0f5f7ad6bdf54e81fd0
Member <burrellmc43@gmail.com>
2026-03-08 00:17:24 -0600

    Update user.js

--- src/user.js
+++ src/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true, sparse: true },
-    password: { type: String, required: true },
+    password: { type: String, required: function () { return !!this.email; }, default: null },
     license_key: { type: String, required: false, unique: true, sparse: true, default: null },
     hwid: { type: String, default: null },
 
