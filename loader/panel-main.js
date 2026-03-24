134b86943a7e7c2e18c7a38c2d40ad035501ff75
Member <burrellmc43@gmail.com>
2026-03-24 00:28:14 -0500

    Update panel-main.js

--- loader/panel-main.js
+++ loader/panel-main.js
@@ -1,9 +1,12 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 
+const LOGIN_WINDOW_SIZE = { width: 470, height: 650 };
+const PANEL_WINDOW_SIZE = { width: 1100, height: 840 };
+
 function createWindow() {
     const win = new BrowserWindow({
-        width: 1100,
-        height: 840,
+        width: LOGIN_WINDOW_SIZE.width,
+        height: LOGIN_WINDOW_SIZE.height,
         frame: false,
         transparent: true,
         resizable: false,
@@ -15,9 +18,19 @@ function createWindow() {
 
     win.loadFile('admin.html');
 
-    // Handle Buttons
+    const resizeWindow = (size) => {
+        if (!win || win.isDestroyed()) {
+            return;
+        }
+
+        win.setSize(size.width, size.height, true);
+        win.center();
+    };
+
     ipcMain.on('admin-close', () => app.quit());
     ipcMain.on('admin-min', () => win.minimize());
+    ipcMain.on('admin-expand', () => resizeWindow(PANEL_WINDOW_SIZE));
+    ipcMain.on('admin-collapse', () => resizeWindow(LOGIN_WINDOW_SIZE));
 }
 
 app.whenReady().then(createWindow);
